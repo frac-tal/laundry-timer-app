4480dd47d504b55ee179e5bf4d8191051c5ed1aa
frac-tal <12370607+frac-tal@users.noreply.github.com>
2018-01-20 16:48:28 +0200

    create timed notification by planPicker and button

--- app/src/main/java/com/example/android/washingmachinetimer/MainActivity.java
+++ app/src/main/java/com/example/android/washingmachinetimer/MainActivity.java
@@ -20,16 +20,34 @@ public class MainActivity extends AppCompatActivity {
     Long timeAtStart;
     NumberPicker planPicker;
     String[] planNames;
+    int[] planTimes;
+    int selectedPlanTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        planNames = getResources().getStringArray(R.array.plans);
+        planTimes = new int[planNames.length];
+        for (int i=0; i<planNames.length; i++) {
+            String[] sepperate = planNames[i].split("\\|", 2);
+            //planTimes[i] = Integer.parseInt(sepperate[0]) * 1000 * 60;
+            planTimes[i] = Integer.parseInt(sepperate[0]) * 1000; // just for now, waiting seconds instead of minutes.
+            planNames[i] = sepperate[1];
+        }
+
         planPicker = findViewById(R.id.plan_picker);
         planPicker.setMinValue(0);
         planPicker.setMaxValue(planNames.length - 1);
         planPicker.setDisplayedValues(planNames);
+
+        planPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                selectedPlanTime = planTimes[newVal];
+            }
+        });
 
         mButton = findViewById(R.id.only_button);
         mButton.setOnClickListener(new View.OnClickListener() {
@@ -37,10 +55,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 timeAtStart = new GregorianCalendar().getTimeInMillis();
                 Context mContext = getApplicationContext();
-                int timeToWait = 1000*10;
                 Intent intentAlarm = new Intent(mContext, AlarmReceiver.class);
                 AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-                alarmManager.set(AlarmManager.RTC_WAKEUP, (timeAtStart + timeToWait), PendingIntent.getBroadcast
+                alarmManager.set(AlarmManager.RTC_WAKEUP, (timeAtStart + selectedPlanTime), PendingIntent.getBroadcast
                         (mContext, 1, intentAlarm, PendingIntent.FLAG_UPDATE_CURRENT));
             }
         });
