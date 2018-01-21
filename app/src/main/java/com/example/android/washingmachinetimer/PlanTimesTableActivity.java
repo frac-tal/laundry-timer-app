60eaa43e36ca74eca970d6371bf43bb3ec0bddfd
frac-tal <12370607+frac-tal@users.noreply.github.com>
2018-01-22 00:33:27 +0200

    info now table

--- app/src/main/java/com/example/android/washingmachinetimer/PlanTimesTableActivity.java
+++ app/src/main/java/com/example/android/washingmachinetimer/PlanTimesTableActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class PlanTimesTableActivity extends AppCompatActivity {
@@ -12,19 +14,29 @@ public class PlanTimesTableActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_times_table);
-        LayoutInflater inflater = getLayoutInflater();
 
-        LinearLayout plansList = findViewById(R.id.plans_list);
-        String[] seperated = new String[2];
+        TableLayout plansList = findViewById(R.id.plans_list);
+        String[] seperated;
         String[] plans = getResources().getStringArray(R.array.plans);
+        int stdPadding = (int) getResources().getDimension(R.dimen.standard);
+        int fontSize = (int) getResources().getDimension(R.dimen.plan_list_text_size);
+
         for (int i=0; i<plans.length; i++) {
             seperated = plans[i].split("\\|", 2);
-            LinearLayout entry = (LinearLayout) inflater.inflate(R.layout.plan_entry_layout, null);
-            TextView planNameTextView = entry.findViewById(R.id.plan_name);
-            TextView planTimeTextView = entry.findViewById(R.id.plan_time);
+            TableRow row = new TableRow(this);
+            TextView planNameTextView = new TextView(this);
+            TextView planTimeTextView = new TextView(this);
             planTimeTextView.setText(String.format(getString(R.string.plan_time_entry), seperated[0]));
             planNameTextView.setText(seperated[1]);
-            plansList.addView(entry);
+
+            planNameTextView.setPadding(stdPadding, stdPadding, stdPadding, stdPadding);
+            planTimeTextView.setPadding(stdPadding, stdPadding, stdPadding, stdPadding);
+            planNameTextView.setTextSize(fontSize);
+            planTimeTextView.setTextSize(fontSize);
+
+            row.addView(planNameTextView);
+            row.addView(planTimeTextView);
+            plansList.addView(row);
         }
     }
 }
